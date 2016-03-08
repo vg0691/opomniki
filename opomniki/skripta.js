230c21d828d2296afcc82caf890ecb97e388212a
Vanesa Gradišar <vg0691@student.uni-lj.si>
2016-03-08 09:42:04 +0000

    popravek opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -17,7 +17,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		document.querySelector("#opomniki").innerHTML += " \
-			<div class='opomnik'> \
+			<div class='opomniki senca rob'> \
             <div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
             <div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div> \
 			</div>";
@@ -37,6 +37,14 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas == 0) {
+				var naziv = document.querySelector("#naziv_opomnika").innerHTML;
+				alert("Opomnik!\n\nZadolžitev '" + naziv + "' je potekla!");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			} else {
+				casovnik.innerHTML = cas - 1;
+			}
+			
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
