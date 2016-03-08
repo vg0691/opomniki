347859e21848518037d521fcf18fd91e9f6a203c
Vanesa Gradišar <vg0691@student.uni-lj.si>
2016-03-08 09:31:28 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -8,7 +8,23 @@ window.addEventListener('load', function() {
 	}
 	
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo); //na objekt gumba dodamo listener
+	
+	//Dodaj opomnike
+	var dodajOpomnik = function(event) {
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
 		
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		document.querySelector("#opomniki").innerHTML += " \
+			<div class='opomnik'> \
+            <div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
+            <div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div> \
+			</div>";
+	
+	}
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
